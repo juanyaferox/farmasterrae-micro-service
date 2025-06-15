e4ce0bb954fef0f399b9c4db4935605fd76ef2c6
juanyaferox <juaniago2001@gmail.com>
2025-06-16 01:53:58 +0200

    clean code in create user case.

--- user-management/user-command-service/src/main/java/dev/juanyaferox/user/application/exception/EmailAlreadyExistsException.java
+++ user-management/user-command-service/src/main/java/dev/juanyaferox/user/application/exception/EmailAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package dev.juanyaferox.user.application.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus (HttpStatus.BAD_REQUEST)
 public class EmailAlreadyExistsException extends RuntimeException {
     public EmailAlreadyExistsException(String message) {
         super(message);
--- user-management/user-command-service/src/main/java/dev/juanyaferox/user/application/usecase/CreateUserUseCase.java
+++ user-management/user-command-service/src/main/java/dev/juanyaferox/user/application/usecase/CreateUserUseCase.java
@@ -1,14 +1,14 @@
 package dev.juanyaferox.user.application.usecase;
 
 import dev.juanyaferox.user.application.dto.CreateUserCommand;
+import dev.juanyaferox.user.application.exception.EmailAlreadyExistsException;
+import dev.juanyaferox.user.application.exception.UserAlreadyExistsException;
 import dev.juanyaferox.user.application.mapper.UserMapper;
 import dev.juanyaferox.user.domain.model.User;
 import dev.juanyaferox.user.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class CreateUserUseCase {
@@ -26,11 +26,11 @@ public class CreateUserUseCase {
     public void execute(CreateUserCommand command) {
 
         if (userRepository.findByUsername(command.getUsername()).isPresent())
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuario ya registrado");
+            throw new UserAlreadyExistsException("Nombre de usuario ya registrado");
 
         if (userRepository.findByEmail(command.getEmail()).isPresent())
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email ya registrado");
-        
+            throw new EmailAlreadyExistsException("Email ya registrado");
+
         User user = userMapper.commandToDomain(command)
                 .withPassword(passwordEncoder.encode(command.getPassword()));
         userRepository.save(user);
